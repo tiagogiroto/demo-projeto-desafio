709b603877679f2ea1b2dbc49506cbbe0dc8a356
Tiago Giroto Neto <tiago.giroto.neto1@gmail.com>
2021-11-18 10:39:48 -0300

    Progresso db

--- src/main/java/com/example/demo/Dispositivo.java
+++ src/main/java/com/example/demo/Dispositivo.java
@@ -1,5 +1,12 @@
 package com.example.demo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,13 +16,27 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "dispositivo")
 public class Dispositivo {
     
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
     private Long deviceId;
+
+    @Column(name="name")
     private String name;
+
+    @Column(name="mac")
     private Integer mac;
+
+    @Column(name="email")
     private String email;
+
+    @Column(name="latitude")
     private String latitude;
+
+    @Column(name="longitude")
     private String longitude;
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,24 @@
 DROP TABLE IF EXISTS dispositivo;
-
 CREATE TABLE dispositivo (
-  deviceId INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50),
-  mac int,
-  email VARCHAR(50),
-  latitude VARCHAR(80),
-  longitude VARCHAR(80),
+id bigint auto_increment,
+name VARCHAR(50),
+mac int,
+email VARCHAR(50),
+latitude VARCHAR(80),
+longitude VARCHAR(80)
 );
 
-INSERT INTO dispositivo(deviceId, name, mac, email, latitude, longitude)
+INSERT INTO dispositivo(name, mac, email, latitude, longitude)
 VALUES ('tiago', 123456, 'ttiago@teste.com', '12313.112', '23156.22');
+
+INSERT INTO dispositivo(name, mac, email, latitude, longitude)
+VALUES ('jorge', 2432, 'jorge@teste.com', '4324.112', '55.22');
+
+INSERT INTO dispositivo(name, mac, email, latitude, longitude)
+VALUES ('john', 45353, 'john@teste.com', '543.112', '44.22');
+
+INSERT INTO dispositivo(name, mac, email, latitude, longitude)
+VALUES ('jan', 65346, 'jan@teste.com', '4324.112', '777.22');
+
+INSERT INTO dispositivo(name, mac, email, latitude, longitude)
+VALUES ('aaa', 423423, 'aaa@teste.com', '432.112', '2222.22');
